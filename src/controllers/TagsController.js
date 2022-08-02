042ec2ca3d0683d3c503980165d2ce35b10e9b6f
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2022-08-02 16:00:35 -0300

    API_Node

---
+++ src/controllers/TagsController.js
@@ -0,0 +1,15 @@
+const knex = require ("../database/knex");
+
+class TagsCoontroller{
+
+    async index(request, response){
+        const {user_id} = request.params;
+
+        const tags = await knex("tags").where({ user_id })
+
+        return response.json(tags)
+    }
+}
+
+
+module.exports = TagsCoontroller;
---
+++ src/routes/tags.routes.js
@@ -0,0 +1,13 @@
+const {Router}= require ("express");
+const TagsController= require ("../controllers/TagsController");
+
+const tagsRoutes = Router();
+
+
+const tagsController = new TagsController();
+
+tagsRoutes.get("/:user_id", tagsController.index );
+
+
+
+module.exports = tagsRoutes;
