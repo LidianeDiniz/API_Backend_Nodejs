b55e22cae44a0a1da3bc0026f9bd698b705308aa
lidiane <lidianefernandesdiniz.lfd@gamil.com>
2022-08-20 20:56:22 -0300

    Automated tests

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,28 +1,20 @@
 const { hash, compare } = require ("bcryptjs");
 const AppError = require("../utils/AppError");
 const sqliteConnection = require ("../database/sqlite");
+const UserRepository = require("../repositories/UserRepository")
+const UserCreateService = require ("../services/UserCreateService")
+
 const { request, response } = require("express");
 
 
 class UsersController {
    async create(request, response){
       const { name, email, password } = request.body;
-      const database = await sqliteConnection();
 
-      const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+      const userRepository = new UserRepository();
+      const userCreateService = new UserCreateService(userRepository);
+      await userCreateService.execute({name, email, password});
 
-      if(checkUserExists){
-        throw new AppError("Este e-mail já esta em uso!")
-      }
-
-      const hashedPassword = await hash(password, 8);
-
-      await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?) ",
-      [name, email, hashedPassword]
-      
-      ) 
-      
-      
       return response.status(201).json();
     }
     
